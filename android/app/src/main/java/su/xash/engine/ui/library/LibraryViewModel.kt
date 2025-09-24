b86138f64dddaeae1995d156ab1962394cf7a907
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-09-25 02:42:14 +0300

    Update LibraryViewModel.kt

--- android/app/src/main/java/su/xash/engine/ui/library/LibraryViewModel.kt
+++ android/app/src/main/java/su/xash/engine/ui/library/LibraryViewModel.kt
@@ -90,7 +90,7 @@ class LibraryViewModel(application: Application) : AndroidViewModel(application)
 
     private fun isGameDirectory(dirName: String): Boolean {
         val gameDirs = arrayOf("valve", "cstrike", "czero", "gearbox", "bshift", "dmc", "hldms", "tfc", "wanted")
-        return gameDirs.contains(dirName.toLowerCase())
+        return gameDirs.contains(dirName.lowercase()) // lowercase() kullanıldı
     }
 
     fun setSelectedGame(game: Game) {
