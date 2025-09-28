9e7ce1582543328d742ab9511afd06755a494acb
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-09-28 17:55:52 +0300

    Update gl_local.h

--- ref/gl/gl_local.h
+++ ref/gl/gl_local.h
@@ -789,7 +789,7 @@ extern convar_t	gl_msaa;
 extern convar_t	gl_stencilbits;
 extern convar_t	gl_overbright;
 extern convar_t gl_fog;
-
+extern convar_t cl_seebehindwall;
 extern convar_t	r_lighting_extended;
 extern convar_t	r_lighting_ambient;
 extern convar_t	r_studio_lambert;
