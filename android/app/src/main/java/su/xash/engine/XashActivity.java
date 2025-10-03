ef6dc37a24ab703066b0ab9c593b837a3f32b023
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-10-03 23:46:07 +0300

    Update XashActivity.java

--- android/app/src/main/java/su/xash/engine/XashActivity.java
+++ android/app/src/main/java/su/xash/engine/XashActivity.java
@@ -3,19 +3,14 @@ package su.xash.engine;
 import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.content.SharedPreferences;
 import android.provider.Settings.Secure;
 import android.util.Log;
-import android.util.DisplayMetrics;
 import android.view.KeyEvent;
-import android.view.Window;
 import android.view.WindowManager;
-import android.view.View;
 
 import org.libsdl.app.SDLActivity;
 
@@ -30,9 +25,6 @@ public class XashActivity extends SDLActivity {
     private String mPackageName;
     private static final String TAG = "XashActivity";
     private SharedPreferences mPreferences;
-    private int mCustomWidth;
-    private int mCustomHeight;
-    private float mResolutionScale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,125 +32,12 @@ public class XashActivity extends SDLActivity {
 
         mPreferences = getSharedPreferences("app_preferences", MODE_PRIVATE);
 
-        mCustomWidth = Integer.parseInt(mPreferences.getString("resolution_width", "0"));
-        mCustomHeight = Integer.parseInt(mPreferences.getString("resolution_height", "0"));
-        mResolutionScale = Float.parseFloat(mPreferences.getString("resolution_scale", "1.0"));
-
-        overrideDisplayMetrics();
-
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
         }
 
         AndroidBug5497Workaround.assistActivity(this);
-    }
-
-    private void overrideDisplayMetrics() {
-        DisplayMetrics realMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(realMetrics);
-        
-        int targetWidth;
-        int targetHeight;
-        
-        if (mCustomWidth > 0 && mCustomHeight > 0) {
-            targetWidth = mCustomWidth;
-            targetHeight = mCustomHeight;
-            Log.d(TAG, "Overriding display to: " + targetWidth + "x" + targetHeight);
-        } else if (mResolutionScale != 1.0f) {
-            targetWidth = (int)(realMetrics.widthPixels / mResolutionScale);
-            targetHeight = (int)(realMetrics.heightPixels / mResolutionScale);
-            Log.d(TAG, "Overriding display with scale " + mResolutionScale + ": " + targetWidth + "x" + targetHeight);
-        } else {
-            Log.d(TAG, "Using native resolution: " + realMetrics.widthPixels + "x" + realMetrics.heightPixels);
-            return;
-        }
-
-        DisplayMetrics fakeMetrics = new DisplayMetrics();
-        fakeMetrics.widthPixels = targetWidth;
-        fakeMetrics.heightPixels = targetHeight;
-        fakeMetrics.density = realMetrics.density;
-        fakeMetrics.densityDpi = realMetrics.densityDpi;
-        fakeMetrics.scaledDensity = realMetrics.scaledDensity;
-        fakeMetrics.xdpi = realMetrics.xdpi;
-        fakeMetrics.ydpi = realMetrics.ydpi;
-
-        overrideResourcesMetrics(fakeMetrics);
-        
-        notifySDLAboutResolution(targetWidth, targetHeight, realMetrics.widthPixels, realMetrics.heightPixels);
-    }
-
-    private void overrideResourcesMetrics(DisplayMetrics metrics) {
-        try {
-            Resources resources = getResources();
-            resources.getDisplayMetrics().setTo(metrics);
-
-            Resources activityResources = super.getResources();
-            activityResources.getDisplayMetrics().setTo(metrics);
-
-            Log.d(TAG, "Resources metrics overridden to: " + metrics.widthPixels + "x" + metrics.heightPixels);
-            
-        } catch (Exception e) {
-            Log.e(TAG, "Failed to override resources metrics: " + e.getMessage());
-        }
-    }
-
-    private void notifySDLAboutResolution(int width, int height, int deviceWidth, int deviceHeight) {
-        try {
-            float refreshRate = getWindowManager().getDefaultDisplay().getRefreshRate();
-            
-            SDLActivity.nativeSetScreenResolution(width, height, deviceWidth, deviceHeight, refreshRate);
-            
-            nativeSetenv("SDL_VIDEO_WINDOW_WIDTH", String.valueOf(width));
-            nativeSetenv("SDL_VIDEO_WINDOW_HEIGHT", String.valueOf(height));
-            nativeSetenv("XASH_RESOLUTION_WIDTH", String.valueOf(width));
-            nativeSetenv("XASH_RESOLUTION_HEIGHT", String.valueOf(height));
-            
-            Log.d(TAG, "Notified SDL about resolution: " + width + "x" + height);
-            
-        } catch (Exception e) {
-            Log.e(TAG, "Error notifying SDL: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public Resources getResources() {
-        Resources res = super.getResources();
-        if ((mCustomWidth > 0 || mResolutionScale != 1.0f)) {
-            DisplayMetrics metrics = res.getDisplayMetrics();
-            
-            if ((mCustomWidth > 0 && metrics.widthPixels != mCustomWidth) || 
-                (mResolutionScale != 1.0f && metrics.widthPixels != (int)(getRealWidth() / mResolutionScale))) {
-                overrideDisplayMetrics();
-            }
-        }
-        return res;
-    }
-
-    private int getRealWidth() {
-        DisplayMetrics realMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getRealMetrics(realMetrics);
-        return realMetrics.widthPixels;
-    }
-
-    private int getRealHeight() {
-        DisplayMetrics realMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getRealMetrics(realMetrics);
-        return realMetrics.heightPixels;
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        
-        if (hasFocus) {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    overrideDisplayMetrics();
-                }
-            }, 100);
-        }
     }
 
     @Override
@@ -305,6 +184,22 @@ public class XashActivity extends SDLActivity {
         String argv = getIntent().getStringExtra("argv");
         if (argv == null) argv = "-console -log";
 
+        String widthStr = mPreferences.getString("resolution_width", "0");
+        String heightStr = mPreferences.getString("resolution_height", "0");
+        String scaleStr = mPreferences.getString("resolution_scale", "1.0");
+
+        int width = Integer.parseInt(widthStr);
+        int height = Integer.parseInt(heightStr);
+        float scale = Float.parseFloat(scaleStr);
+
+        if (width > 0 && height > 0) {
+            argv += " -width " + width + " -height " + height;
+            Log.d(TAG, "Added resolution args: -width " + width + " -height " + height);
+        } else if (scale != 1.0f) {
+            argv += " -width " + (int)(getRealWidth() / scale) + " -height " + (int)(getRealHeight() / scale);
+            Log.d(TAG, "Added scaled resolution args with scale: " + scale);
+        }
+
         String globalArgs = getGlobalArguments();
         if (!globalArgs.isEmpty()) {
             Log.d(TAG, "Global arguments found: " + globalArgs);
@@ -326,16 +221,19 @@ public class XashActivity extends SDLActivity {
                 argv += " -dll @hl";
         }
 
-        applyFinalResolutionSettings();
-
         Log.d(TAG, "Final argv: " + argv);
         return argv.split(" ");
     }
 
-    private void applyFinalResolutionSettings() {
-        if (mCustomWidth > 0 && mCustomHeight > 0) {
-            nativeSetenv("SDL_VIDEO_FORCE_WIDTH", String.valueOf(mCustomWidth));
-            nativeSetenv("SDL_VIDEO_FORCE_HEIGHT", String.valueOf(mCustomHeight));
-        }
+    private int getRealWidth() {
+        android.util.DisplayMetrics realMetrics = new android.util.DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(realMetrics);
+        return realMetrics.widthPixels;
+    }
+
+    private int getRealHeight() {
+        android.util.DisplayMetrics realMetrics = new android.util.DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(realMetrics);
+        return realMetrics.heightPixels;
     }
 }
