7a96396268d77b896841709f03f6bb1f6567aea2
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-10-05 19:46:13 +0300

    Update gl_export.h

--- ref/gl/gl_export.h
+++ ref/gl/gl_export.h
@@ -35,6 +35,12 @@ GNU General Public License for more details.
 	#define XASH_GLES 1
 #endif // XASH_GLES3COMPAT
 
+qboolean R_CreateScaleRenderTarget( int width, int height );
+void R_DestroyScaleRenderTarget( void );
+void R_BindRenderTargetForScene( void );
+void R_BlitScaleRenderTargetToScreen( int screen_w, int screen_h );
+qboolean R_SetDisplayTransform( ref_screen_rotation_t rotate, int offset_x, int offset_y, float scale_x, float scale_y );
+
 typedef uint GLenum;
 typedef byte GLboolean;
 typedef uint GLbitfield;
@@ -55,12 +61,6 @@ typedef int GLsizeiptrARB;
 typedef char GLcharARB;
 typedef uint GLhandleARB;
 typedef float GLmatrix[16];
-
-qboolean R_CreateScaleRenderTarget( int width, int height );
-void R_DestroyScaleRenderTarget( void );
-void R_BindRenderTargetForScene( void );
-void R_BlitScaleRenderTargetToScreen( int screen_w, int screen_h );
-qboolean R_SetDisplayTransform( ref_screen_rotation_t rotate, int offset_x, int offset_y, float scale_x, float scale_y );
 
 #define GL_MODELVIEW			0x1700
 #define GL_PROJECTION			0x1701
