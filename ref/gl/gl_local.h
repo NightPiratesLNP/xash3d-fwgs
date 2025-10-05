30dabe75f6d6f1c2f40f435e5324ed9860e09c3a
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-10-05 20:48:03 +0300

    Update gl_local.h

--- ref/gl/gl_local.h
+++ ref/gl/gl_local.h
@@ -546,12 +546,6 @@ void Mod_SetOrthoBounds( const float *mins, const float *maxs );
 void R_NewMap( void );
 void CL_AddCustomBeam( cl_entity_t *pEnvBeam );
 
-qboolean R_CreateScaleRenderTarget( int width, int height );
-void R_DestroyScaleRenderTarget( void );
-void R_BindRenderTargetForScene( void );
-void R_BlitScaleRenderTargetToScreen( int screen_w, int screen_h );
-qboolean R_SetDisplayTransform( ref_screen_rotation_t rotate, int offset_x, int offset_y, float scale_x, float scale_y );
-
 //
 // gl_opengl.c
 //
