ae66abc1246f7f694d5f491eb888b5d20fc4b387
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-10-05 19:31:48 +0300

    Update gl_export.h

--- ref/gl/gl_export.h
+++ ref/gl/gl_export.h
@@ -56,7 +56,9 @@ typedef char GLcharARB;
 typedef uint GLhandleARB;
 typedef float GLmatrix[16];
 
-qboolean R_BindRenderTargetForScene( void );
+qboolean R_CreateScaleRenderTarget( int width, int height );
+void R_DestroyScaleRenderTarget( void );
+void R_BindRenderTargetForScene( void );
 void R_BlitScaleRenderTargetToScreen( int screen_w, int screen_h );
 qboolean R_SetDisplayTransform( ref_screen_rotation_t rotate, int offset_x, int offset_y, float scale_x, float scale_y );
 
