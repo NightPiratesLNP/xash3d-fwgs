933fd2584e274e57e09333b1dbdfb71cfcd58887
hasandramali <93338836+hasandramali@users.noreply.github.com>
2026-04-20 23:14:59 +0300

    Update build_android.sh

--- scripts/gha/build_android.sh
+++ scripts/gha/build_android.sh
@@ -3,7 +3,7 @@
 unset ANDROID_SDK_ROOT
 export JAVA_HOME=$GITHUB_WORKSPACE/java
 export ANDROID_HOME=$GITHUB_WORKSPACE/sdk
-export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/tools/bin
+export PATH=$PATH:$JAVA_HOME/bin:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/tools/bin
 
 pushd android || exit 1
 
@@ -25,4 +25,3 @@ mkdir -p artifacts/
 
 mv android/app/build/outputs/apk/continuous/app-continuous-signed.apk artifacts/xash3d-fwgs-android.apk
 tar -cJvf artifacts/xash3d-fwgs-android-mappings.tar.zst -C android/app/build/outputs/mapping/continuous '.'
-
